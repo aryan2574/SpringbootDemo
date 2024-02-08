d19d92a5afd55400ded0051ac973a88475a61615
aryan2574 <ayush.aryan94@yahoo.com>
2024-02-08 20:26:50 +0530

    Implemented Exception Handling

--- src/main/java/com/springexample/Springboot/tutorial/service/DepartmentService.java
+++ src/main/java/com/springexample/Springboot/tutorial/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.springexample.Springboot.tutorial.service;
 
 import com.springexample.Springboot.tutorial.entity.Department;
+import com.springexample.Springboot.tutorial.error.DepartmentNotFoundException;
 
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface DepartmentService {
 
     public List<Department> fetchDepartmentList();
 
-    public Department fetchDepartmentById(Long departmentId);
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
 
     public void deleteDepartmentById(Long departmentId);
 
